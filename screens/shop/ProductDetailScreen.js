4a027ec0058a0d213a8edf1c0a0568743339b7c6
Ali Ossaily <ali.ossaily@Almajmoua.org>
2021-03-21 13:25:30 +0200

    navigate to productDetailScreen

---
+++ screens/shop/ProductDetailScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text,View , Image,Button,ScrollView,StyleSheet} from 'react-native'
+
+const ProductDetailScreen = props => {
+    return(
+        <View>
+            <Text>Product Details</Text>
+        </View>
+    )
+}
+
+export default ProductDetailScreen;
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import {FlatList, Text} from 'react-native'
+import { FlatList, Text } from 'react-native'
 import { useSelector } from 'react-redux'
 import ProductItem from '../../components/shop/ProductItem'
 const ProductsOverviewScreen = props => {
     const Products = useSelector(state => state.products.availableProducts);
 
-    return <FlatList 
-    data={Products} 
-    keyExtractor={item => item.id}
-    renderItem={itemData =>
-         <ProductItem 
-         image={itemData.item.imageUrl}
-         title={itemData.item.title}
-         price={itemData.item.price}
-         onViewDetails={() => {}}
-         onAddToCart={() => {}}
-          />       
-        }   
+    return <FlatList
+        data={Products}
+        keyExtractor={item => item.id}
+        renderItem={itemData =>
+            <ProductItem
+                image={itemData.item.imageUrl}
+                title={itemData.item.title}
+                price={itemData.item.price}
+                onViewDetail={() => {
+                    // props.navigation.navigate('ProductDetail')
+                    props.navigation.navigate({ routeName: 'ProductDetail' })
+                }}
+                onAddToCart={() => { }}
+            />
+        }
     />
 }
-ProductsOverviewScreen.navigationOptions={
-    headerTitle:'All Products'
+ProductsOverviewScreen.navigationOptions = {
+    headerTitle: 'All Products'
 }
 export default ProductsOverviewScreen;

bd668b21f011f08a173dd36394e6085be6ea9257
Ali Ossaily <ali.ossaily@Almajmoua.org>
2021-03-29 19:15:02 +0300

    add Header Button

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {createStore , combineReducers} from 'redux';
-import {Provider} from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
 import ShopNavigator from './navigation/ShopNavigator';
+import *  as Font from 'expo-font'
+import AppLoading from 'expo-app-loading';
+//import { composeWithDevTools } from 'redux-devtools-extension'; // use this only while development 
 
 const rootReducer = combineReducers({
-  products: productsReducer
+  products: productsReducer,
+  cart: cartReducer
 })
 const store = createStore(rootReducer);
-
+//const store = createStore(rootReducer,composeWithDevTools);
+// const fetchFonts = () =>{
+//   return Font.loadAsync({
+//     'open-sans' : require('./assets/fonts/OpenSans-Regular.ttf'),
+//     'open-sans-bold' : require('./assets/fonts/OpenSans-Bold.ttf')
+//   })
+// }
 export default function App() {
-  return (
-    <Provider store={store}>
-     <ShopNavigator/>
-    </Provider>
-  );
+  // const [fontLoaded , setFontLoaded] = useState(false);
+
+  // if(!fontLoaded){
+  //   return <AppLoading
+  //   startAsync = {fetchFonts}
+  //   onFinish={() =>{setFontLoaded(true)}}
+  //   onError = {(err) => console.log(err)}
+  //   />
+  // }
+  return (<Provider store={store}><ShopNavigator /></Provider>);
 }
 
 const styles = StyleSheet.create({
--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Text, View, Image, Button, ScrollView, StyleSheet } from 'react-native'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import Colors from '../../constants/Colors';
+import * as cartActions from '../../store/actions/cart'
+
 
 
 const ProductDetailScreen = props => {
     const productId = props.navigation.getParam('productId')
     const selectedProduct = useSelector(state => state.products.availableProducts.find(prod => prod.id === productId));
-
+    const dispatch = useDispatch();
     return (
-         <ScrollView>
-             <Image style={styles.image} source={{uri : selectedProduct.imageUrl}}/>
-             <View style={styles.actions}>
-             <Button color={Colors.primary} title='Add To Cart' onPress={() => {}} />
-             </View>
-             <Text  style={styles.price}>${selectedProduct.price}</Text>
-             <Text  style={styles.description}>{selectedProduct.description}</Text>
-         </ScrollView>
+        <ScrollView>
+            <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+            <View style={styles.actions}>
+                <Button color={Colors.primary} title='Add To Cart' onPress={() => {
+                     dispatch(cartActions.addToCart(selectedProduct)) 
+                     }} />
+            </View>
+            <Text style={styles.price}>${selectedProduct.price}</Text>
+            <Text style={styles.description}>{selectedProduct.description}</Text>
+        </ScrollView>
     )
 }
 ProductDetailScreen.navigationOptions = navData => {
@@ -26,24 +30,24 @@ ProductDetailScreen.navigationOptions = navData => {
 }
 
 const styles = StyleSheet.create({
-    image:{
-        width:'100%',
-        height:300
+    image: {
+        width: '100%',
+        height: 300
     },
-    price:{
-        fontSize:20,
-        color:'#888',
-        textAlign:'center',
-        marginVertical:20
+    price: {
+        fontSize: 20,
+        color: '#888',
+        textAlign: 'center',
+        marginVertical: 20
     },
-    description:{
-        fontSize:14,
-        textAlign:'center',
-        marginHorizontal:20
+    description: {
+        fontSize: 14,
+        textAlign: 'center',
+        marginHorizontal: 20
     },
-    actions:{
-        marginVertical:10,
-        alignItems:'center'
+    actions: {
+        marginVertical: 10,
+        alignItems: 'center'
     }
 })
 export default ProductDetailScreen;

d0369b71e9932436f20e295cb36431a838c09d7b
Ali Ossaily <ali.ossaily@Almajmoua.org>
2021-03-30 16:46:59 +0300

    url fix header button bug

--- components/UI/HeaderButton.js
+++ components/UI/HeaderButton.js
@@ -3,7 +3,8 @@ import {Platform} from 'react-native';
 import { HeaderButton } from "react-navigation-header-buttons";
 import { Ionicons  } from "@expo/vector-icons";
 import Colors from '../../constants/Colors'
-
+//solve error header button
+//https://stackoverflow.com/questions/66535013/typeerror-0-native-usetheme-is-not-a-function-in-0-native-usetheme
 const CustomHeaderButton = props => {
     return(
     <HeaderButton 

d52a72ca13619e5e2161ecf7928f7dc26ceb1059
Ali Ossaily <ali.ossaily@Almajmoua.org>
2021-03-30 18:06:23 +0300

    remove item

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {View, Text, FlatList, Button, StyleSheet} from 'react-native'
-import {useSelector} from 'react-redux';
+import {useSelector,useDispatch} from 'react-redux';
 import Colors from '../../constants/Colors';
-
+import CartItem from '../../components/shop/CartItem'
+import * as cartActions from '../../store/actions/cart'
 const CartScreen = props => {
     const cartTotalAmount = useSelector(state => state.cart.totalAmount);
     const cartItems = useSelector(state => {
         const transformedCartItems = [];
+        console.log(state.cart.items)
         for(const key in state.cart.items){
             transformedCartItems.push({
                productId: key,
@@ -16,17 +18,28 @@ const CartScreen = props => {
                sum: state.cart.items[key].sum
             })
         }
-        return transformedCartItems;
+        return transformedCartItems.sort((a,b) => a.productId  > b.productId ? 1 :-1);
     });
+
+    const dispatch = useDispatch();
 return(
 <View style={styles.screen}>
     <View style={styles.summary} >
         <Text style={styles.summaryText}>Total: <Text style={styles.amount}>${cartTotalAmount.toFixed(2)}</Text></Text>
         <Button color={Colors.accent} title="Order Now" disabled={cartItems.length === 0}/>
     </View>
-    <View>
-        <Text>Cart Items</Text>
-    </View>
+<FlatList
+data={cartItems}
+keyExtractor={item => item.productId}
+renderItem={itemData => <CartItem 
+ quantity={itemData.item.quantity}
+ title={itemData.item.productTitle}
+ amount={itemData.item.sum}
+ onRemove={() => {
+     dispatch(cartActions.removeFromCart(itemData.item.productId))
+ }}
+ />}
+/>
 </View>
 )
 }
--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,5 +1,6 @@
-import { ADD_TO_CART } from "../actions/cart";
+import { ADD_TO_CART , REMOVE_FROM_CART } from "../actions/cart";
 import CartItem from '../../models/cart-item'
+import { ActivityIndicatorComponent } from "react-native";
 
 
 const initialState = {
@@ -29,6 +30,28 @@ export default (state = initialState, action) => {
                 items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
                 totalAmount: state.totalAmount + prodPrice
             }
+            case REMOVE_FROM_CART:
+                const selectedCartItem = state.items[action.pid];
+                const currentQty = selectedCartItem.quantity;
+                let updatedCartItems;
+                if(currentQty > 1){ // eza L product fe menno marteen wkabas delete badna na2eson mesh nem7eehon
+                   const updatedCartItem = new CartItem(
+                    selectedCartItem.quantity - 1,
+                    selectedCartItem.productPrice,
+                    selectedCartItem.productTitle,
+                    selectedCartItem.sum - selectedCartItem.productPrice
+                   )
+                   updatedCartItems={...state.items , [action.pid] : updatedCartItem}
+                }
+                else{ // eza l product fe menno wa7ad bdna  nem7ee
+                    updatedCartItems={...state.items};
+              delete updatedCartItems[action.pid];
+                }
+                return{
+                    ...state,
+                    items: updatedCartItems,
+                    totalAmount: state.totalAmount - selectedCartItem.productPrice
+                }
     }
     return state;
 }
